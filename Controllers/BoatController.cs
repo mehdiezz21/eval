7af70f71343230e70929efbaa9aa82e148ffbdf9
mehdi ezzahi <ezzahi.mehdi21@gmail.com>
2021-04-01 09:51:31 +0200

    add api

---
+++ Controllers/BoatController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Boat.Models;
+
+namespace Boat.Controllers
+{
+    public class BoatController : Controller
+    {
+        private const string _cookieKey = "stations";
+
+        public IActionResult Liste()
+        {
+            var stations = GetBoatFromApi();
+
+
+
+            return View(stations);
+        }
+
+        public IActionResult Carte()
+        {
+            var stations = GetBoatFromApi();
+            return View(stations);
+        }
+
+
+
+        private static List<Boats> GetBoatFromApi()
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Boats>>(stringResult.Result);
+                return result;
+            }
+        }
+    }
+}
+
---
+++ Models/Boat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boat.Models
+{
+    public class Boats
+    {
+        public string boat_name { get; set; }
+        public string closing_type { get; set; }
+        public DateTime closing_date { get; set; }
+        public DateTime reopening_date { get; set; }
+    }
+
+}
